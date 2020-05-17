db9be5d96e08e098e75a6e1960ea2439cb58aa22
vla7n1k <myhuskyteam1@gmail.com>
2020-05-17 12:43:17 +0300

    Added simple sort by brand and price

--- views/shop/Shop.vue
+++ views/shop/Shop.vue
@@ -1,6 +1,17 @@
 <template>
     <v-container fluid>
-        <v-row>
+        <v-row class="justify-center">
+            <v-col cols="4">
+                <v-select
+                        :items="orderOptions"
+                        label="Order By"
+                        item-text="name"
+                        item-value="id"
+                        v-model="selectedOrder"
+                        @input="orderBy(selectedOrder)"
+                        outlined
+                ></v-select>
+            </v-col>
             <v-col cols="12">
                 <app-loader v-if="!loaded"></app-loader>
                 <v-row class="pa-2 justify-center" v-if="products.length > 0">
@@ -23,9 +34,34 @@
         data: () => ({
             serverUrl: process.env.VUE_APP_SERVER_URL,
             loaded: false,
-            products: []
+            products: [],
+            orderOptions: [
+                {id: 0, name: 'Price Ascending'},
+                {id: 1, name: 'Price Descending'},
+                {id: 2, name: 'Brand name A-Z'},
+                {id: 3, name: 'Brand name Z-A'},
+            ],
+            selectedOrder: null
         }),
         methods: {
+            orderBy(id) {
+                switch (id) {
+                    case 0:
+                        this.products.sort((a, b) => (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0));
+                        break;
+                    case 1:
+                        this.products.sort((a, b) => (a.price < b.price) ? 1 : ((b.price < a.price) ? -1 : 0));
+                        break;
+                    case 2:
+                        this.products.sort((a, b) => (a.brand.name > b.brand.name) ? 1 : ((b.brand.name > a.brand.name) ? -1 : 0));
+                        break;
+                    case 3:
+                        this.products.sort((a, b) => (a.brand.name < b.brand.name) ? 1 : ((b.brand.name < a.brand.name) ? -1 : 0));
+                        break;
+                    default:
+                        this.products.sort((a, b) => (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0));
+                }
+            },
             async fetchProducts() {
                 const response = await fetch(`${this.serverUrl}shop`, {
                     method: 'GET',
@@ -51,6 +87,7 @@
                         owner: true
                     }
                 });
+                this.orderBy();
             }
         },
         beforeMount() {
